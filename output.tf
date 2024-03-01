273ea1147cdfbdb6c0e30177c1f965db382f401b
root <root@ip-172-31-37-186.ec2.internal>
2024-03-01 14:56:24 +0000

    Changes from Raghu

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@ output "subnets" {
   value = module.subnets
 }
 output "public_subnets" {
-  value = local.public_subnet_ids
+  value = local.public_subnets_ids
 }
 output "vpc_id" {
   value = aws_vpc.main.id
